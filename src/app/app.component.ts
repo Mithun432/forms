a2566d26bfa79f0fc9577923f6a273c9d604c9ce
Mithun Pes <mithunpes180@gmail.com>
2024-04-04 19:06:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'task8';
+title="task8";
+myform:any={
+  username:'',
+  email:'',
+  address:'',
+  dob:'',
+  password:'',
+  phone:''
+};
+  onSubmit(){
+    console.log(this.myform);
+    alert("Successfully Submitted");
+  }
 }
